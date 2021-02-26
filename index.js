a40c2334182d8aedbc020b5e53fcea8b4682400f
puifaiba <puifai.ba@gmail.com>
2021-02-25 21:37:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const text_to_change = document.getElementById("text");
+  text_to_change.textContent = "This is really cool!";
+});
